7aacf392512315ded9a330517c22731853ce77fb
DylanDrechsel <DrechselDylan@gmail.com>
2021-02-19 09:37:33 -0500

    for loop to add basic event listener to the four buttons

---
+++ app.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('#button')
+
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        console.log("worked")
+    })
+}
