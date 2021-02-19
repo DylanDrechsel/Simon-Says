0814205432e993c92852bfeed454a763a7107477
DylanDrechsel <DrechselDylan@gmail.com>
2021-02-19 10:09:32 -0500

    for loop that adds eventlisteners to the buttons that pushes the value int the playerArray

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const buttons = document.querySelectorAll('#button')
+const buttons = document.getElementsByClassName('simon-button')
+const playerArray = [];
+const computerArray = [];
+let audioFiles = [];
 
-
+// for loop that adds eventListeners to the buttons that pushes the value into the playerArray
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
-        console.log("worked")
+        playerArray.push(i)
     })
 }
