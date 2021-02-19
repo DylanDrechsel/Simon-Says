e97ce58e397384e3aee184aa50140cbf7d853125
DylanDrechsel <DrechselDylan@gmail.com>
2021-02-19 10:26:55 -0500

    adding audio plays to the button

--- app.js
+++ app.js
@@ -3,9 +3,32 @@ const playerArray = [];
 const computerArray = [];
 let audioFiles = [];
 
+// adding audio files to the audioFiles array
+audioFiles[0] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+audioFiles[1] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+audioFiles[2] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+audioFiles[3] = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+
 // for loop that adds eventListeners to the buttons that pushes the value into the playerArray
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         playerArray.push(i)
+        audioFiles[i].play()
     })
+}
+
+//
+function generateComputerChoice () {
+    choice = Math.floor(Math.random() * 4)
+    computerArray.push(choice)
+}
+
+generateComputerChoice()
+
+function checkArrays (computerArray, playerArray) {
+    for (let i = 0; i < computerArray.length; i++) {
+        console.log(computerArray[i])
+        console.log(playerArray[i])
+    }
 }
