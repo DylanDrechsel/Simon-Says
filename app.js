7435a2809d40a24f2c399f3079b9f91156fa3ec8
DylanDrechsel <DrechselDylan@gmail.com>
2021-02-22 14:31:40 -0500

    completed the function to find the interval and append it to the webpage when the answer is correct

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // grabs all the buttons for simon says
 const buttons = document.getElementsByClassName('simon-button')
-const checkArraysButton = document.getElementById('checkArray')
+const intervalDisplay = document.getElementById('intervalDisplay')
 
 // arrays to hold the player and computer choices
 let playerArray = [];
@@ -28,20 +28,33 @@ for (let i = 0; i < buttons.length; i++) {
 
         // adds funtionality to buttons that will check if the player & computer arrays match to determine game state
         if (playerArray.length === computerArray.length) {
-					if (JSON.stringify(playerArray) == JSON.stringify(computerArray)) {
-						console.log('hit');
-						playerArray = []
-						generateComputerChoice();
-					} else {
-						console.log('GAMEOVER');
-					}
-				}
+
+            // if player array equal computer array 
+            if (JSON.stringify(playerArray) == JSON.stringify(computerArray)) {
+                // console.log('hit');
+
+                // sets the innterText to the music interval if answer is correct
+                intervalDisplay.innerText = intervals[choice];
+
+                // clears player array for next round
+                playerArray = []
+
+                // calls the generateComputerChoice function to continue game
+                generateComputerChoice();
+
+            // if player array doesnt equal computer array
+            } else {
+
+                // you failed game over
+                console.log('GAMEOVER');
+            }
+        }
     })
 }
 
 // generates the computers choice and calls the playComputerArracy function 
 function generateComputerChoice() {
-    choice = Math.floor(Math.random() * 4)
+    choice = Math.floor(Math.random() * 8)
     computerArray.push(choice)
     length = computerArray.length
     // let counter = 0
@@ -54,26 +67,42 @@ function playComputerArray(length, counter) {
     // console.log(length)
     if (length > 0) {
         length = length - 1
-        
+
         // using recursion to avoid javasctipts synchronousity
         setTimeout(() => {
             num = computerArray[counter]
             counter = counter + 1
             // console.log(num)
-            audioFiles[num].play();
+            // audioFiles[num].play();
             playComputerArray(length, counter);
         }, 1000);
     } else {
         printDistance(computerArray, counter)
         counter = 0;
         // console.log('end of loop')
-        
+
     }
 }
 
-function printDistance (computerArray, counter) {
+// used to find the distances between notes so we can find the interval to display on the screen
+function printDistance(computerArray, counter) {
+
     // console.log(counter)
     // console.log(computerArray[counter - 2])
     // console.log(computerArray[counter - 1])
-    console.log(computerArray[counter - 1] - computerArray[counter - 2])
+    choice = computerArray[counter - 1] - computerArray[counter - 2]
+    if (choice < 0) {
+        console.log(choice)
+        choice = choice / -1
+        console.log(choice)
+        console.log(intervals[choice])
+    } else {
+        console.log(choice)
+        console.log(intervals[choice])
+    }
+
 }
+
+const intervals = ['same note', 'Major 2nd', 'Major 3rd', 'Perfect 4th', 'Perfect 5th', 'Major 6th', 'Major 7th', 'Octive']
+
+const decendingIntervals = []
